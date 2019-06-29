677ad3313ba3f17fc212fb7f1ab456bafcaca2a4
Anirudhsfc <anirudhsfc@gmail.com>
2019-06-29 15:21:38 +0800

    Form added

--- FrontEnd/fh-app/src/App.js
+++ FrontEnd/fh-app/src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import './sass/main.scss'; //to check
 import MainPage from './views/MainPage/MainPage' 
 import Footer from './components/Footer/footer' //to check
+import Form from './components/Form/form'
+import Navbar from './components/NavBar/navbar'
 
 function App() {
   return (
     <div>
+      <Navbar/>
+      {/* <Form/> */}
    <MainPage/>
    <Footer/>
    </div>
