ca956a3390f6aab1fc37ed1f1f6ba78ab767fa4a
Raghu Saxena <u3543617@connect.hku.hk>
2019-06-30 02:26:36 +0800

    Did the shit for getting 5 vals for some fuckall graph

--- BackEnd/v1/api_route.js
+++ BackEnd/v1/api_route.js
@@ -3,8 +3,10 @@ let router = express.Router();
 
 let send_info = require('./controllers/send_info');
 let get_pools = require('./controllers/get_pools');
+let get_risk_class_percent = require('./controllers/get_risk_class_percent');
 
 router.post('/send_info', send_info);
 router.get('/get_pools', get_pools);
+router.get('/get_risk_class_percent', get_risk_class_percent);
 
 module.exports = router;
