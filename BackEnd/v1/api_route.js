68586338552fcd667c9587a8c15463594de2978e
Raghu Saxena <u3543617@connect.hku.hk>
2019-06-30 01:46:57 +0800

    Added the fucking endpoint to show the pools crap

--- BackEnd/v1/api_route.js
+++ BackEnd/v1/api_route.js
@@ -2,7 +2,9 @@ let express = require('express');
 let router = express.Router();
 
 let send_info = require('./controllers/send_info');
+let get_pools = require('./controllers/get_pools');
 
 router.post('/send_info', send_info);
+router.get('/get_pools', get_pools);
 
 module.exports = router;
